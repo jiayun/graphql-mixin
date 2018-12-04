11ca4ce6151e5050286a0deebc4a1106a69b1b1a
Jiayun Zhou <jiayun@gmail.com>
2018-12-04 15:40:40 +0800

    Clean.

--- graphql-mixin-ktjs/src/mixin.kt
+++ graphql-mixin-ktjs/src/mixin.kt
@@ -8,11 +8,7 @@ fun main(args: Array<String>) {
 
 @JsName("mixin")
 fun mixin(args: Array<String>): String {
-//    args.forEach { println(it) }
-
     val schema = readAndCombineFiles(args)
-//    println(schema)
-
     val (schema2, mixins) = extractMixinDefinitions(schema)
     return injectMixin(schema2, mixins)
 }
@@ -37,35 +33,21 @@ fun extractMixinDefinitions(schema: String): Pair<String, Map<String, String>> {
     var matchResult = regex.find(schema)
 
     while (matchResult != null) {
-//        println(matchResult.value)
-
         val (_, name, definition) = matchResult.destructured
         map[name] = definition
-//        println("comment: $comment\nname: $name\ndefinition:\n$definition")
 
         matchResult = matchResult.next()
     }
 
     val result = regex.replace(schema, "")
-//    println(result)
-
     return Pair(result, map)
 }
 
 fun injectMixin(schema: String, definitions: Map<String, String>): String {
     val regex = """\s*?@mixin\s(\w*).*""".toRegex()
 
-    var matchResult = regex.find(schema)
-
-    while (matchResult != null) {
-//        println(matchResult.value)
-        matchResult = matchResult.next()
-    }
-
     return regex.replace(schema) {
         val (name) = it.destructured
-//        println("mixin name: $name")
-//        println(definitions[name]!!)
         "\n" + definitions[name]!!
     }
 }
--- graphql-mixin-ktjvm/src/mixin.kt
+++ graphql-mixin-ktjvm/src/mixin.kt
@@ -1,11 +1,7 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-//    args.forEach { println(it) }
-
     val schema = readAndCombineFiles(args)
-//    println(schema)
-
     val (schema2, mixins) = extractMixinDefinitions(schema)
     val schema3 = injectMixin(schema2, mixins)
 
@@ -30,35 +26,21 @@ fun extractMixinDefinitions(schema: String): Pair<String, Map<String, String>> {
     var matchResult = regex.find(schema)
 
     while (matchResult != null) {
-//        println(matchResult.value)
-
-        val (comment, name, definition) = matchResult.destructured
+        val (_, name, definition) = matchResult.destructured
         map[name] = definition
-//        println("comment: $comment\nname: $name\ndefinition:\n$definition")
 
         matchResult = matchResult.next()
     }
 
     val result = regex.replace(schema, "")
-//    println(result)
-
     return Pair(result, map)
 }
 
 fun injectMixin(schema: String, definitions: Map<String, String>): String {
     val regex = """\s*?@mixin\s(\w*).*""".toRegex()
 
-    var matchResult = regex.find(schema)
-
-    while (matchResult != null) {
-//        println(matchResult.value)
-        matchResult = matchResult.next()
-    }
-
     return regex.replace(schema) {
         val (name) = it.destructured
-//        println("mixin name: $name")
-//        println(definitions[name]!!)
         "\n" + definitions[name]!!
     }
 }
